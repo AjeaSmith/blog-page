b3613ac39cfd2385799a2bded42039a670a98148
ajea <smithajea@rocketmail.com>
2025-06-07 14:17:14 -0400

    04-home-hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,25 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()],
+	vite: {
+		plugins: [tailwindcss()],
 	},
 
-  integrations: [react()],
+	integrations: [
+		react(),
+		icon({
+			svgoOptions: {
+				plugins: [
+					{
+						name: "convertColors",
+						params: { currentColor: true },
+					},
+				],
+			},
+		}),
+	],
 });
