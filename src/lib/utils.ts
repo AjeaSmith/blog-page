72e208ddfb8a33f80ac2762311c582fb5efb77af
ajea <smithajea@rocketmail.com>
2025-06-09 21:24:46 -0400

    07-blog-page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+export function formatTime(time: string) {
+	const date = new Date(time);
+	const formatted = date.toLocaleDateString("en-US", {
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+	});
+	return formatted;
 }
