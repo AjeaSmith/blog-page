34c9c1e604edc8a1ec8822534622f65346a80b84
ajea <smithajea@rocketmail.com>
2025-06-20 06:04:50 -0400

    testing: navlinks in SSR

---
+++ src/components/NavLink.jsx
@@ -0,0 +1,35 @@
+import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
+
+export default function NavLink({ href, label, isMobile }) {
+	const [isActive, setIsActive] = useState(false);
+
+	useEffect(() => {
+		if (window.location.pathname === href) {
+			setIsActive(true);
+		}
+	}, [href]);
+
+	return isMobile ? (
+		<li
+			id="mobileNavLink"
+			className={cn(
+				"py-3 border-b border-b-(--border) transition-colors duration-500 ease-in last:border-b-0",
+				isActive ? "text-(--activeLink) text-18-semi" : "text-(--text-color)"
+			)}
+		>
+			<a href={href}>{label}</a>
+		</li>
+	) : (
+		<a
+			href={href}
+			className={
+				isActive
+					? "underline decoration-blue-500 decoration-3"
+					: "hover:underline hover:decoration-3 hover:decoration-blue-500"
+			}
+		>
+			{label}
+		</a>
+	);
+}
